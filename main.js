2d8382b1e9bf3044670d86b1dd32838b5008c519
Michael-Fares <mjfares@gmail.com>
2021-05-06 09:53:55 -0500

    Tic Tac Toe terminal app complete

--- main.js
+++ main.js
@@ -33,25 +33,76 @@ const printBoard = () => {
 }
 
 const horizontalWin = () => {
-  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")) {
+    return true
+  } else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")) {
+    return true
+  } else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const verticalWin = () => {
-  // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")) {
+    return true
+  } else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")) {
+    return true
+  } else if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const diagonalWin = () => {
-  // Your code here to check for diagonal wins
+// Your code here to check for diagonal wins
+if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")) {
+  return true
+} else if ((board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X") || (board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O")) {
+  return true
+} else {
+  return false
+}
 }
 
 const checkForWin = () => {
-  // Your code here call each of the check for types of wins
+  if(horizontalWin || verticalWin || diagonalWin) {
+    return true
+  } else {
+    getPrompt()
+  }
+}
+
+const changePlayer = () => {
+  if (playerTurn === 'X') {
+    playerTurn = 'O'
+  } else {
+    playerTurn = 'X'
+  }
 }
 
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
-  // then check for a win
-}
+  // check to see if the current space is NOT already occupied. If so, then:
+  // put the playerTurn marker (whether X or O) at chosen row and column 
+  
+  if (board[row][column] === ' ') {
+    board[row][column] = playerTurn
+    } else {
+      return 'You cannot play on an occupied space!!'
+    }
+    // change turn to the next player
+    changePlayer()
+    console.log(board)
+    console.log(playerTurn)
+      // then check for a win
+      checkForWin()
+      
+  }
+
+
 
 const getPrompt = () => {
   printBoard();
